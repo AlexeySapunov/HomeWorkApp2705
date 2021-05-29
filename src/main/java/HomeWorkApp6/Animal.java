c8d4b5a0ec8d9553a84cc373a13bb19db99ffcb5
Alexey <blacka8607@rambler.ru>
2021-05-30 02:27:38 +0300

    Create Animal.class

--- src/main/java/HomeWorkApp6/Animal.java
+++ src/main/java/HomeWorkApp6/Animal.java
@@ -1,7 +1,24 @@
 package HomeWorkApp6;
 
-public class Animal {
+public abstract class  Animal {
 
+    protected int run;
+    protected int swim;
 
+    private static int countAnimal;
 
+    public Animal(int run, int swim) {
+        countAnimal++;
+        this.swim = swim;
+        this.run = run;
+    }
+
+    public static int getCountAnimal() {
+        return countAnimal;
+    }
+
+    public abstract void run(int r);
+
+    public abstract void swim(int s);
 }
+
--- src/main/java/HomeWorkApp6/Main.java
+++ src/main/java/HomeWorkApp6/Main.java
@@ -9,6 +9,15 @@ public class Main {
 //   Результатом выполнения действия будет печать в консоль. (Например, dogBobik.run(150); -> 'Бобик пробежал 150 м.');
 //3. У каждого животного есть ограничения на действия (бег: кот 200 м., собака 500 м.; плавание: кот не умеет плавать, собака 10 м.).
 //4. * Добавить подсчет созданных котов, собак и животных.
+        Dog dogBobik = new Dog(600, 20);
+        Cat catBarsik = new Cat(300, 100);
 
+        System.out.println("Создано животных - " + Animal.getCountAnimal());
+        System.out.println("Создано собак - " + Dog.getCountDog());
+        dogBobik.run(500);
+        dogBobik.swim(10);
+        System.out.println("Создано кошек - " + Cat.getCountCat());
+        catBarsik.run(200);
+        catBarsik.swim(20);
     }
 }
